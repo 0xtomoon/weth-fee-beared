0286b1cab3e3ff6da0f5f4bddf275b304058882c
shoyano <shoyano.soft@gmail.com>
2021-10-08 01:11:31 +0700

    remove safemath

--- contracts/WETH.sol
+++ contracts/WETH.sol
@@ -1,12 +1,9 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 contract WETH is ERC20, ReentrancyGuard {
-    using SafeMath for uint256;
-
     uint256 private constant FEE = 1; // 0.01%
     uint256 private constant PERCENT = 10000; // 100%
     uint256 private totalFeeAmount;
@@ -16,9 +13,9 @@ contract WETH is ERC20, ReentrancyGuard {
     function mint() public payable nonReentrant {
         uint256 amount = msg.value;
 
-        uint256 feeAmount = amount.mul(FEE).div(PERCENT);
-        uint256 amountToMint = amount.sub(feeAmount);
-        totalFeeAmount = totalFeeAmount.add(feeAmount);
+        uint256 feeAmount = (amount * FEE) / PERCENT;
+        uint256 amountToMint = amount - feeAmount;
+        totalFeeAmount = totalFeeAmount + feeAmount;
 
         _mint(msg.sender, amountToMint);
     }
@@ -35,10 +32,9 @@ contract WETH is ERC20, ReentrancyGuard {
             "Cannot withdraw 0"
         );
 
-        uint256 amountToReturn = totalFeeAmount.mul(balanceOf(msg.sender)).div(
-            totalSupply()
-        );
-        totalFeeAmount = totalFeeAmount.sub(amountToReturn);
+        uint256 amountToReturn = (totalFeeAmount * (balanceOf(msg.sender))) /
+            totalSupply();
+        totalFeeAmount = totalFeeAmount - amountToReturn;
         (bool success, ) = msg.sender.call{value: amountToReturn}("");
         require(success);
     }
